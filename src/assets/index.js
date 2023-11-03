07915a79a6887dc142b2670c3a9fa8a986721bc6
Jane Ezike <jane.ezike@etranzact.com>
2023-11-03 09:27:28 +0100

    created the image folder

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/prefer-default-export */
+
+export { default as Bigi } from './svgs/bigi.svg';
+export { default as Dubic } from './svgs/dubicmalt.svg';
+export { default as Know } from './svgs/knowcube.svg';
+export { default as Logo } from './svgs/logo.svg';
+export { default as Milo } from './svgs/milo.svg';
+export { default as RoyalStallion } from './svgs/royalstallion.svg';
+export { default as Schewapp } from './svgs/schewepp.svg';
+export { default as HeroImage } from './svgs/heroimage.svg';
+export { default as  Sprite } from './svgs/sprite.svg';
+export { default as WhiteOat } from './svgs/whiteoat.svg';
