5cea0affc033b1751cbd2d63d224fc93903b9032
Jane Ezike <jane.ezike@etranzact.com>
2023-11-07 13:24:34 +0100

    edited card width

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -5,9 +5,9 @@ import styles from './style.module.css';
 
 function Card({ image, text, price, imagealt }) {
   return (
-    <div className="w-[92%] mx-auto">
+    <div>
       <div className={styles.card}>
-        <img src={image} alt={imagealt} />
+        <img src={image} alt={imagealt} className="w-[92%] mx-auto" />
       </div>
       <p className={styles.text}>{text}</p>
       <p className={styles.price}>{price}</p>
