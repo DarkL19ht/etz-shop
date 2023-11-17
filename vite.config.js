f00d68b1de1b36cbe14eab560c4b83c5298da1fb
JaneEzike <Jane.Ezike@etranzact.com>
2023-11-17 02:38:17 -0800

    set up path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 4500,
+  },
+});
