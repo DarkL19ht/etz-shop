a761aa7d9b326d93eeb3547507507679b0ecfe73
JaneEzike <Jane.Ezike@etranzact.com>
2023-11-24 08:33:36 +0100

    changed branch name

---
+++ src/utils/helpers.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+export const delay = (seconds) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, seconds * 1000);
+    });
+  };
