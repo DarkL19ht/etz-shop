42ecf8fd4ad2e742e151e5869bac11d089bd4a55
JaneEzike <Jane.Ezike@etranzact.com>
2023-11-24 09:34:50 +0100

    added the modal

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-/* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
