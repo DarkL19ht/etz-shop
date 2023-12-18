5bbde480409e75fc4b4b0e40319ca226f930a35d
Nky237 <ezikenkemdilijane@gmail.com>
2023-12-18 12:29:58 +0100

    added isloading state

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,26 +1,26 @@
-import { cva } from 'class-variance-authority';
-import React, { ButtonHTMLAttributes, FC, forwardRef } from 'react';
-import { cn } from '@/utils/helpers';
-import Loader from '../loader';
+import { cva } from "class-variance-authority";
+import React, { ButtonHTMLAttributes, FC, forwardRef } from "react";
+import { cn } from "@/utils/helpers";
+import Loader from "../loader";
 
 const buttonVariants = cva(
   `px-3 py-2 rounded-xl transition-all hover:ring-2 ring-indigo-300 hover:text-white`,
   {
     variants: {
       variant: {
-        primary: 'bg-blue-400',
-        danger: 'bg-red-500',
-        lemony: 'bg-lemon-100 text-white',
-        alert: 'bg-yellow-500',
+        primary: "bg-blue-400",
+        danger: "bg-red-500",
+        lemony: "bg-lemon-200 text-white",
+        alert: "bg-yellow-500",
       },
       size: {
-        small: 'py-2 px-4',
-        large: 'text-xl py-3 px-6',
+        small: "py-2 px-4",
+        large: "text-xl py-3 px-6",
       },
     },
     defaultVariants: {
-      size: 'small',
-      variant: 'primary',
+      size: "small",
+      variant: "primary",
     },
   }
 );
@@ -37,7 +37,9 @@ const Button = forwardRef(
       <button
         ref={ref}
         type="button"
-        className={cn(buttonVariants({ className, variant, size }))}
+        className={cn(buttonVariants({ className, variant, size }), {
+          "bg-lemon-100": isLoading === true,
+        })}
         {...props}
       >
         <div className="flex items-center justify-center gap-3 align-middle">
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,10 @@
-import Nav from '@/components/features/Nav';
-import Hero from '@/components/features/Hero';
-import Sale from '@/components/features/Sale';
-import Catalog from '@/components/features/Catalog';
-import Pagination from '@/components/features/Pagination';
-import Button from '@/components/ui/button/Button';
-import Loader from '@/components/ui/loader';
+import Nav from "@/components/features/Nav";
+import Hero from "@/components/features/Hero";
+import Sale from "@/components/features/Sale";
+import Catalog from "@/components/features/Catalog";
+import Pagination from "@/components/features/Pagination";
+import Button from "@/components/ui/button/Button";
+import Loader from "@/components/ui/loader";
 export default function index() {
   return (
     <main className="px-5 py-5 text-sm md:px-10  ">
@@ -13,7 +13,7 @@ export default function index() {
       <Sale />
       <Catalog />
       <Pagination />
-      <Button variant={'lemony'} size={'large'} isLoading={true}>
+      <Button variant={"lemony"} size={"large"} isLoading={false}>
         <h2 className="font-bold">please wait </h2>
         <h1 className="pb-2 font-bold">...</h1>
       </Button>
